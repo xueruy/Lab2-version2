ed17a10e121e73141485cc7023d9bc6722c6b728
xueruy <seansea@seaseandeMacBook-Pro.local>
2016-10-15 14:52:29 -0700

    Lab2 changes
    This is the new change

---
+++ callfk.m
@@ -0,0 +1,14 @@
+%EE 209AS Lab 1 Forward Kinematics 
+% Tzu-Wei Chuang, Xuerui Yan
+
+%Transformation_01: d_1 = d_2 = d_3 = 5 cm, theta = 45 degree. 
+w = fk(0, 0, 5, 0);
+x = fk(0, 0, 5, 0);
+y = fk(0, 0, 5, 0);
+Ehfiwhfjdknjke
+
+fkresult = w*x*y*z
+
+Adding this text
+display('this is Ryan edit')
+
